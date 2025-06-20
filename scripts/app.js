0dad83d7851d2697b946c195b3e38dea36f6464b
JMaxRowe <jmaxrowe@gmail.com>
2025-06-20 13:37:31 +0100

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,54 @@
+
+// ? Store the <body> element inside a const called "body"
+
+
+
+// ? Create a <h1> element and store it in a const called "heading".
+
+
+
+// ? Using innerHTML give the text "I am a heading" to the "heading" element.
+
+
+
+// ? Append the "heading" element to "body" element.
+
+
+
+// ? Set "heading" text colour to grey
+
+
+
+// ? Give "heading" a border of 2px solid green
+
+
+
+// ? Give "heading" 20px of padding
+
+
+
+// ? Use JavaScript to find the height of the "heading" element (including the padding and border) and then console log it. (You should get 81).
+
+
+// ? Create a <div> and store it in a const called "box"
+
+
+// ?  Add the string "I am a div" inside the "box" div
+
+
+// ? Append "box" to the body of the HTML
+
+
+// ? Give "box" the class of "box"
+
+
+// ? Give "box" the id of "single"
+
+
+// ? Console.log the child elements of the "body" we saved to a variable earlier
+
+
+// ? Give "box" the data attribute of "data-id='1'"
+
+
+// ? Find the vertical offset position of "box" (how far down it is from the top of the page) and console log it. (You should get 124).
